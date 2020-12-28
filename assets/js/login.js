da0b040fc1c907d6ae30e831eec4f575a3d2a52c
fbq <1453050414@qq.com>
2020-12-28 20:55:55 +0800

    init:提交了初始化文件

---
+++ assets/js/login.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('#link_reg').on('click', function () {
+        $('.box').hide()
+        $('.box-zhu').show()
+    })
+    $('#link_age').on('click', function () {
+        $('.box').show()
+        $('.box-zhu').hide()
+    })
+
+})
